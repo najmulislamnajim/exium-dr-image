a6d2d465e338c819171837e4189dcad6e6308f6c
najmulislamnajim <ninbd17@gmail.com>
2025-04-23 15:46:13 +0600

    add url

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('upload', views.upload, name='upload'),
     path('download/all', views.download, name='download_all'),
+    path('territories/', views.territory_list_page, name='territory_list'),
+    path('territory/<str:territory_code>/', views.territory_detail, name='territory_detail'),
+    path('accounts/login/', views.login_view, name='login'),
+    path('accounts/logout/', views.user_logout, name='logout'),
+    path('',views.home, name='home'),
 ]
